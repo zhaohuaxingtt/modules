dfc92fa1cc78f8c85b2c7af8cc2c5b1551cc46cd
周玉松 <yuszhou@deloitte.com.cn>
2021-04-21 20:24:48 +0800

    cache code

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
+/*
+ * @Author: your name
+ * @Date: 2021-04-14 15:44:20
+ * @LastEditTime: 2021-04-21 20:24:14
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \front-modules\babel.config.js
+ */
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
-};
+  presets: [
+    '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      "component",
+      {
+        "libraryName": "element-ui",
+        "styleLibraryName": "theme-chalk"
+      }
+    ]
+  ]
+}
