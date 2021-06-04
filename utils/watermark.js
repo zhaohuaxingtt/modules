5a3f211ca081ff0af4426e1962748bb1ce0344e4
zhonganjing <1qaz!QAZ>
2021-06-04 10:38:20 +0800

    add the wartermark

--- utils/watermark.js
+++ utils/watermark.js
@@ -1,55 +1,81 @@
 /**
  * str 水印内容
+ *  option: {
+ *      width: 水印宽度,
+        height: 水印高度,
+        rotate: 角度旋转,
+        font: 字体，默认15 vedana,
+        fillStyle: 背景颜色#4444,
+        textAlign: 文字位置,
+        textBaseline: 文字线,
+        bottom: 底部距离,
+        right: 右部距离,
+        opacity: 透明度,
+        position: 位置,
+        zIndex: 页面索引,
+        repeat: 重复
+ *  }
+ *
  * @param str 水印内容
- * @param appendId 附着的区域id
- * @param width 宽度
- * @param height 高度
+ * @param option
  * @returns {string}
  */
-let setWatermark = (str, appendId, width, height) => {
+let setWatermark = (str, option) => {
     let id = '1.23452384164.123412415'
     if (document.getElementById(id) !== null) {
         document.body.removeChild(document.getElementById(id))
     }
+    let defStyle = {
+        width: 120,
+        height: 40,
+        rotate: -20,
+        font: '15px Vedana',
+        fillStyle: '#444444',
+        textAlign: 'center',
+        textBaseline: 'Middle',
+        bottom: '50px',
+        right: '50px',
+        opacity: '0.15',
+        position: 'fixed',
+        zIndex: '999999',
+        repeat: true
 
+    };
+    defStyle = Object.assign({}, defStyle, option || {});
     let can = document.createElement('canvas')
     // 设置canvas画布大小
-    can.width = width || 120
-    can.height = height || 40
+    can.width = defStyle.width
+    can.height = defStyle.height
 
     let cans = can.getContext('2d')
-    cans.rotate(-20 * Math.PI / 180) // 水印旋转角度
-    cans.font = '15px Vedana'
+    cans.rotate(defStyle.rotate * Math.PI / 180) // 水印旋转角度
+    cans.font = defStyle.font
     cans.fillStyle = '#666666'
-    cans.textAlign = 'center'
-    cans.textBaseline = 'Middle'
+    cans.textAlign = defStyle.textAlign
+    cans.textBaseline = defStyle.textBaseline
     cans.fillText(str, can.width / 2, can.height) // 水印在画布的位置x，y轴
 
     let div = document.createElement('div')
     div.id = id
     div.style.pointerEvents = 'none'
-    div.style.top = '40px'
-    div.style.left = '0px'
-    div.style.opacity = '0.15'
-    div.style.position = 'fixed'
-    div.style.zIndex = '100000'
+    div.style.bottom = defStyle.bottom
+    div.style.right = defStyle.right
+    div.style.opacity = defStyle.opacity
+    div.style.position = defStyle.position
+    div.style.zIndex = defStyle.zIndex
     div.style.width = document.documentElement.clientWidth + 'px'
     div.style.height = document.documentElement.clientHeight + 'px'
-    div.style.background = 'url(' + can.toDataURL('image/png') + ') left top repeat'
-    if (appendId && document.getElementById(appendId)) {
-        document.getElementById(appendId).appendChild(div)
-    } else {
-        document.body.appendChild(div);
-    }
+    div.style.background = 'url(' + can.toDataURL('image/png') + ') right bottom ' + (defStyle.repeat ? 'repeat' : 'no-repeat')
+    document.body.appendChild(div);
     return id
 }
 
 // 添加水印方法
-export const setWaterMark = (str, appendId, width, height) => {
-    let id = setWatermark(str, appendId, width, height)
+export const setWaterMark = (str, option) => {
+    let id = setWatermark(str, option)
     setInterval(() => {
         if (document.getElementById(id) === null) {
-            id = setWatermark(str, appendId, width, height);
+            id = setWatermark(str, option);
         }
     }, 500);
 }
