8bf4c7d43518ee506e5477b8fc2361f07ce58592
wentliao <wentliao@deloitte.com.cn>
2021-08-31 15:58:13 +0800

    update 样件费

--- web/quotationdetail/components/sample/data.js
+++ web/quotationdetail/components/sample/data.js
@@ -1,17 +1,17 @@
 /*
  * @Author: ldh
  * @Date: 2021-04-23 14:30:32
- * @LastEditors: ldh
- * @LastEditTime: 2021-04-27 12:08:45
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-08-31 15:45:27
  * @Description: In User Settings Edit
  * @FilePath: \front-supplier\src\views\rfqManageMent\quotationdetail\components\sample\data.js
  */
 export const sampleTableTitle = [
-  { props: "sampleType", name: "样件", tooltip: true, key: "LK_YANGJIAN" },
+  { props: "sampleType", name: "样件", tooltip: true, key: "LK_YANGJIAN",isAeko:true },
 	{ props: "requiredTime", name: "需要日期(周)", tooltip: true, key: "LK_XUYAORIQIZHOU" },
 	{ props: "quantity", name: "需要数量(件)", tooltip: true, key: "LK_XUYAOSHULIANGJIAN" },
-	{ props: "sampleUnitPrice", name: "工装样件单件费用(RMB)", tooltip: true, key: "LK_GONGZHUANGYANGJIANDANJIANFEIYONGRMB" },
-	{ props: "addionalMouldCost", name: "附加工装样件模具价格(RMB)", tooltip: true, key: "LK_FUJIAGONGZHUANGYANGJIANMUJUJIAGERMB" },
-  { props: "addionalMouldLife", name: "附加工装样件模具寿命(个数)", tooltip: true, key: "LK_FUJIAGONGZHUANGYANGJIANMUJUSHOUMINGGESHU" },
-  { props: "remark", name: "备注", tooltip: true, key: "LK_BEIZHU" },
+	{ props: "sampleUnitPrice", name: "工装样件单件费用(RMB)", tooltip: true, key: "LK_GONGZHUANGYANGJIANDANJIANFEIYONGRMB" ,isAeko:true},
+	{ props: "addionalMouldCost", name: "附加工装样件模具价格(RMB)", tooltip: true, key: "LK_FUJIAGONGZHUANGYANGJIANMUJUJIAGERMB" ,isAeko:true},
+  { props: "addionalMouldLife", name: "附加工装样件模具寿命(个数)", tooltip: true, key: "LK_FUJIAGONGZHUANGYANGJIANMUJUSHOUMINGGESHU" ,isAeko:true},
+  { props: "remark", name: "备注", tooltip: true, key: "LK_BEIZHU" ,isAeko:true},
 ]
