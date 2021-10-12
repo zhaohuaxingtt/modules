1de3ca97fb2e012bf5f729e59f433b2192d2a0ba
周玉松 <yuszhou@deloitte.com.cn>
2021-10-12 20:19:40 +0800

    update code

--- web/inquiryManagement/index.vue
+++ web/inquiryManagement/index.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-10-12 19:55:39
- * @LastEditTime: 2021-10-12 19:59:08
+ * @LastEditTime: 2021-10-12 20:16:46
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \front-modules\web\inquiryManagement\index.vue
@@ -12,7 +12,7 @@
 </iCard>
 </template>
 <script>
-import iCard from 'rise'
+import {iCard} from 'rise'
 export default{
   components:{iCard}
 }
