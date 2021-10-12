1d22b17af6f698ca61657d41970f18c4eb0ab644
LEGION-Y9000P\dingh <dinghao.liu@hotmail.com>
2021-10-12 21:15:54 +0800

    update code

--- web/aeko/quotationdetail/components/mouldInvestmentChange/index.vue
+++ web/aeko/quotationdetail/components/mouldInvestmentChange/index.vue
@@ -1,10 +1,36 @@
 <template>
-  <div></div>
+  <div>
+    <module ref="module" :partInfo="partInfo" :disabled="disabled" />
+    <sourcePartCBD ref="sourcePartCBD" class="margin-top20" :partInfo="partInfo" @quote="handleQuote" :disabled="disabled" />
+  </div>
 </template>
 
 <script>
-export default {
+import module from "./components/module"
+import sourcePartCBD from "./components/sourcePartCBD"
 
+export default {
+  components: { module, sourcePartCBD },
+  props: {
+    partInfo: {
+      type: Object,
+      required: true,
+      default: () => ({})
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    handleQuote(list) {
+      this.$refs.module.addQuote(list)
+    },
+    init() {
+      this.$refs.module.getMoulds()
+      this.$refs.sourcePartCBD.getMoldCbd()
+    }
+  }
 }
 </script>
 
