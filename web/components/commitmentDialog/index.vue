244980f29b4c214361700cfd5996d553dfa1e3d4
diliu <diliu@deloitte.com.cn>
2021-10-28 17:44:03 +0800

    update code

--- web/components/commitmentDialog/index.vue
+++ web/components/commitmentDialog/index.vue
@@ -13,15 +13,15 @@
     </div>
     <template v-if="!disabled" #footer class="footer">
       <el-checkbox class="reviewed" v-model="reviewed">{{ language("WOYIYUEDUBINGJIESHOUYISHANGTIAOKUAN", "我已阅读并接受以上条款") }}</el-checkbox>
-      <iButton :disabled="!reviewed" @click="handleAccept">{{ language("JIESHOU", "接受") }}</iButton>
-      <iButton @click="handleReject">{{ language("JUJUE", "拒绝") }}</iButton>
+      <iButton :disabled="!reviewed" :loading="confirmLoading" @click="handleConfirm(1)">{{ language("JIESHOU", "接受") }}</iButton>
+      <iButton :loading="confirmLoading" @click="handleConfirm(0)">{{ language("JUJUE", "拒绝") }}</iButton>
     </template>
   </iDialog>
 </template>
 
 <script>
 import { iDialog, iSelect, iButton, iMessage } from "rise"
-import { getNoticeDetail } from "@/api/rfqManageMent/quotationdetail"
+import { getNoticeDetail, signNotice } from "@/api/rfqManageMent/quotationdetail"
 
 export default {
   components: { iDialog, iSelect, iButton},
@@ -50,7 +50,18 @@ export default {
   },
   watch: {
     status(nv) {
-      if (nv) { 
+      if (nv) {
+        switch(this.type) {
+          case "RFQ":
+            this.defaultTitle = this.language("XUNJIACHENGNUOSHU", "询价承诺书")
+            break
+          case "CARBON":
+            this.defaultTitle = this.language("KEZAISHENGNENGYUANSHIYONGCHENGNUOSHU", "可再生能源使用承诺书")
+            break
+          default:
+            this.defaultTitle = this.language("CHENGNUOSHU", "承诺书")
+        }
+
         this.getNoticeDetail()
       } else {
         this.defaultTitle = ""
@@ -58,6 +69,7 @@ export default {
         this.disabled = true
         this.reviewed = false
         this.loading = false
+        this.confirmLoading = false
       }
     },
   },
@@ -82,22 +94,12 @@ export default {
       loading: false,
       content: "",
       disabled: true,
-      reviewed: false
+      reviewed: false,
+      confirmLoading: false
     }
   },
   created() {
     this.supplierId = this.userInfo.supplierId || this.$route.query.supplierId
-
-    switch(this.type) {
-      case "RFQ":
-        this.defaultTitle = this.language("XUNJIACHENGNUOSHU", "询价承诺书")
-        break
-      case "CARBON":
-        this.defaultTitle = this.language("KEZAISHENGNENGYUANSHIYONGCHENGNUOSHU", "可再生能源使用承诺书")
-        break
-      default:
-        this.defaultTitle = this.language("CHENGNUOSHU", "承诺书")
-    }
   },
   methods: {
     getNoticeDetail() {
@@ -120,13 +122,32 @@ export default {
       })
       .finally(() => this.loading = false)
     },
-    handleAccept() {
-      this.$emit("accept")
-      this.status = false
-    },
-    handleReject() {
-      this.$emit("reject")
-      this.status = false
+    // 1 接受 / 0 拒绝
+    handleConfirm(status = 0) {
+      this.confirmLoading = true
+
+      signNotice({
+        status,
+        supplierId: this.supplierId,
+        type: this.type
+      })
+      .then(res => {
+        if (res.code == 200) {
+          const message = this.$i18n.locale === "zh" ? res.desZh : res.desEn
+
+          iMessage.success(message)
+
+          if (status === 1) this.$emit("accept") 
+          else this.$emit("reject")
+          
+          this.status = false
+        } else {
+          iMessage.error(message)
+        }
+      })
+      .finally(() => {
+        this.confirmLoading = false
+      })
     }
   }
 };
