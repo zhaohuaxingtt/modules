d82cf23ee842dac7c428680078d359b2636619f4
余继鹏 <917955345@qq.com>
2021-11-16 17:34:40 +0800

    修改小数保留方法

--- web/aeko/quotationdetail/components/data.js
+++ web/aeko/quotationdetail/components/data.js
@@ -34,7 +34,7 @@ export function floatNum(num, minFixed = 2, maxFixed = 4) {
   if (_num === '') return null;
   let floatNum = Number(Number(_num).toFixed(maxFixed));
   let float = floatNum.toString().split(".")[1];
-  const length = Array.isArray(float) ? float.length : 0;
+  const length = typeof float ==='string' ? float.length : 0;
     
   return length < minFixed ? floatNum.toFixed(minFixed) : floatNum;
 }
