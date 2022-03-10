c8031c01fc31f084008db1381e45e951538d7519
qyu <ext-yuqiang@csvw.com>
2022-03-10 15:52:18 +0800

    CRW-6712

--- web/layout/components/notify/index.vue
+++ web/layout/components/notify/index.vue
@@ -1,10 +1,10 @@
 <template>
-	<div class="popupContent">
+	<div class="layout-notify">
 		<notifyDialog
-			v-show="showDialog"
-			:show.sync="showDialog"
+			v-if="dialogVisible"
+			:dialogVisible.sync="dialogVisible"
 			:detail="detail"
-			@is-sure="isSure"
+			@is-sure="handleDialogSure"
 		/>
 	</div>
 </template>
@@ -13,6 +13,7 @@
 import notifyDialog from './notifyDialog.vue'
 import { getPopupList } from '../../api'
 import { getgetPopupSocketMessage } from '@/api/mail'
+import { Notification } from 'element-ui'
 export default {
 	name: 'layoutNotify',
 	components: {
@@ -20,7 +21,7 @@ export default {
 	},
 	data() {
 		return {
-			showDialog: false,
+			dialogVisible: false,
 			detail: {
 				title: '',
 				content: '',
@@ -32,115 +33,72 @@ export default {
 				popupId: '',
 			},
 			popupDataList: [],
-			closeItemList: [],
 			closePopupSocket: null,
-			timer: null,
 		}
 	},
 	mounted() {
 		this.closePopupSocket = getgetPopupSocketMessage((res) => {
-			this.debounce(3000)
+			this.getLatest()
 		})
 	},
 	beforeDestroy() {
-		this.closePopupSocket()
-		if (this.closeItemList) {
-			this.closeItemList.forEach((ele) => {
-				if (ele.notify) {
-					ele.notify.close()
-				}
-			})
+		if (this.closePopupSocket) {
+			this.closePopupSocket()
 		}
-		this.closeItemList = []
-		this.popupDataList = []
-		const elements = document.querySelectorAll('.notifyHandel')
-		if (elements) {
-			elements.forEach((e) => {
-				e.remove()
-			})
-		}
+		this.clearNotify()
 	},
 	created() {
 		this.getLatest()
 	},
 	methods: {
-		isSure(val) {
-			if (val == true) {
-				this.clearNotify()
-			}
-		},
-		openDialog(index) {
-			this.isChecked = false
-			const y = this.popupDataList[index].publishTime.slice(0, 4)
-			const M = this.popupDataList[index].publishTime.slice(5, 7)
-			const d = this.popupDataList[index].publishTime.slice(8, 10)
-			const h = this.popupDataList[index].publishTime.slice(11, 13)
-			const m = this.popupDataList[index].publishTime.slice(14, 16)
-			const time = `${y}年${M}月${d}日 ${h}时${m}分`
-			this.detail = {
-				title: this.popupDataList[index].popupName,
-				content: this.popupDataList[index].content,
-				picUrl: this.popupDataList[index].picUrl,
-				linkUrl: this.popupDataList[index].linkUrl,
-				publishTime: time,
-				popupStyle: this.popupDataList[index].popupStyle,
-				wordAlign: this.popupDataList[index].wordAlign,
-				popupId: this.popupDataList[index].id,
-			}
-			this.showDialog = true
-		},
-		//防抖
-		debounce(delay) {
-			if (this.timer !== null) clearTimeout(this.timer)
-			this.timer = setTimeout(() => {
-				this.iniNotify('pushNew')
-			}, delay)
-		},
 		getLatest() {
-			const accountId = JSON.parse(sessionStorage.getItem('userInfo')).accountId
-			getPopupList(accountId).then((res) => {
-				if (res.code == 200) {
-					let popupDataList = res.data
-					this.popupDataList = popupDataList.reverse()
-					this.iniNotify()
-				} else {
-					this.$message.error(res.desZh)
-				}
-			})
+			console.log('window.requestNotifiy', window.requestNotifiy)
+			if (!window.requestNotifiy) {
+				window.requestNotifiy = true
+				const accountId = JSON.parse(
+					sessionStorage.getItem('userInfo')
+				).accountId
+				getPopupList(accountId)
+					.then((res) => {
+						if (res.code == 200) {
+							let popupDataList = res.data
+							this.popupDataList = popupDataList.reverse()
+							this.showNotify()
+						} else {
+							this.$message.error(res.desZh)
+						}
+					})
+					.finally(() => {
+						window.requestNotifiy = false
+					})
+			}
 		},
-		clearNotify(val) {
-			if (this.closeItemList) {
-				this.closeItemList.forEach((ele) => {
-					if (ele.notify) {
-						ele.notify.close()
-					}
+		clearNotify() {
+			Notification.closeAll()
+			const elements = document.querySelectorAll('.notifyHandel')
+			if (elements) {
+				elements.forEach((e) => {
+					e.remove()
 				})
 			}
-			this.closeItemList = []
-			this.popupDataList = []
-			if (val != 'logout') {
-				this.getLatest()
-			}
 		},
-		iniNotify(pushNew) {
-			let _this = this
-			if (pushNew == 'pushNew') {
-				this.clearNotify()
-			} else {
-				this.popupDataList.forEach((ele, index) => {
-					if (!document.querySelector('.notifyHandel_' + ele.id)) {
-						setTimeout(() => {
-							this.closeItemList[index] = {
-								notify: this.$notify({
-									duration: 0,
-									dangerouslyUseHTMLString: true,
-									customClass: 'notifyHandel notifyHandel_' + ele.id,
-									message: `<div style='display: flex;justify-content: space-between;cursor:pointer'>
+		showNotify() {
+			this.clearNotify()
+			this.popupDataList.forEach((e) => {
+				Notification(this.getNotifyOption(e))
+			})
+		},
+		getNotifyOption(ele) {
+			const _this = this
+			return {
+				duration: 0,
+				dangerouslyUseHTMLString: true,
+				customClass: 'notifyHandel notifyHandel_' + ele.id,
+				message: `<div style='display: flex;justify-content: space-between;cursor:pointer'>
                           <div class="popupLeft" style='width:50px;height:50px; '>
                               <img src="${
-																ele.picUrl
-																	? ele.picUrl
-																	: '/portal/static/img/popupPic.f3ff87ac.png'
+																ele.picUrl ||
+																'/portal/static/img/popupPic.f3ff87ac.png'
 															}" style='width:100%;height:100%; border-radius: 50%;'>
                           </div>
                           <div class="popupRight" style='position:relative;margin-left:20px'>
@@ -154,18 +112,46 @@ export default {
                               >${ele.content}</p>
                           </div>
                       </div>`,
-									position: 'bottom-right',
-									onClick() {
-										_this.openDialog(index)
-									},
-									onClose() {},
-								}),
-								// 'times':0
-							}
-						}, 1)
-					}
-				})
+				position: 'bottom-right',
+				onClick() {
+					_this.openDialog(ele)
+				},
+				onClose() {},
 			}
+		},
+		openDialog(ele) {
+			console.log('open dialog', ele)
+			this.dialogVisible = false
+			const {
+				publishTime,
+				popupName,
+				content,
+				picUrl,
+				linkUrl,
+				popupStyle,
+				wordAlign,
+				id,
+			} = ele
+			const y = publishTime.slice(0, 4)
+			const M = publishTime.slice(5, 7)
+			const d = publishTime.slice(8, 10)
+			const h = publishTime.slice(11, 13)
+			const m = publishTime.slice(14, 16)
+			const time = `${y}年${M}月${d}日 ${h}时${m}分`
+			this.detail = {
+				title: popupName,
+				content,
+				picUrl,
+				linkUrl,
+				publishTime: time,
+				popupStyle: popupStyle,
+				wordAlign: wordAlign,
+				popupId: id,
+			}
+			this.dialogVisible = true
+		},
+		handleDialogSure() {
+			this.clearNotify()
 		},
 	},
 }
