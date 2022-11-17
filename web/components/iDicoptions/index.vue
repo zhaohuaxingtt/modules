61037a5f2288b4fa45e1a9384c3df567647d1d31
余继鹏 <917955345@qq.com>
2022-11-17 12:57:00 +0800

    国际化调整

--- web/components/iDicoptions/index.vue
+++ web/components/iDicoptions/index.vue
@@ -9,103 +9,103 @@
 <template>
   <iSelect
     v-model="data"
-    :placeholder="language('LK_QINGXUANZE','请选择')"
+    :placeholder="language('LK_QINGXUANZE', '请选择')"
     v-bind="$attrs"
     v-on="$listeners"
   >
     <el-option
       v-if="optionAll"
       value=""
-      :label="optionAllText || language('all','全部')"
+      :label="optionAllText || language('all', '全部')"
     ></el-option>
     <el-option
       :value="items.code"
-      :label="items.value"
+      :label="$getLabel ? $getLabel(items.value, item.nameEn) : items.value"
       v-for="(items, index) in options"
       :key="index"
     ></el-option>
   </iSelect>
 </template>
 <script>
-import {iSelect} from 'rise'
-import {selectDictByKeyss} from '@/api/dictionary'
+import { iSelect } from "rise";
+import { selectDictByKeyss } from "@/api/dictionary";
 
 /**
  * @example ./README.me
-*/
+ */
 export default {
-  name:'iDicoptions',
-  components: {iSelect},
+  name: "iDicoptions",
+  components: { iSelect },
   props: {
     // 默认全部文案
     optionAllText: {
       type: String,
-      default: ''
+      default: "",
     },
     value: {
       type: String,
-      default: ''
+      default: "",
     },
     // 接口根据这个optionKey查option
     optionKey: {
       type: String,
-      default: ''
+      default: "",
     },
     // 第一个选项是否展示全部
     optionAll: {
       type: Boolean,
-      default: true
+      default: true,
     },
     // 是否开启语言国际化
     lang: {
       type: Boolean,
-      default: true
+      default: true,
     },
   },
   data() {
     return {
       options: [],
-      data: this.value
-    }
+      data: this.value,
+    };
   },
   watch: {
     data(val) {
       if (val) {
         this.$nextTick(() => {
-          this.data = val
-        })
+          this.data = val;
+        });
       }
-      this.$emit('input', val)
+      this.$emit("input", val);
     },
     optionKey(val) {
       if (val) {
-        this.init()
+        this.init();
       }
     },
     value(val) {
-      this.data = val
-    }
+      this.data = val;
+    },
   },
-  mounted(){
-    this.init()
+  mounted() {
+    this.init();
   },
-  methods:{
+  methods: {
     init() {
-      if (!this.optionKey) return
-      const types = [
-        this.optionKey,
-      ];
+      if (!this.optionKey) return;
+      const types = [this.optionKey];
       selectDictByKeyss(types).then((res) => {
-        const options = res.data && res.data[this.optionKey] || [];
-        this.options = options.map(o => {
-          o.value = o.value || o.name || o.nameEn
-          if (this.lang) o.value = this.$i18n.locale === 'zh' ? o.value : o.nameEn
-          return o
-        })
+        const options = (res.data && res.data[this.optionKey]) || [];
+        this.options = options.map((o) => {
+          o.value = o.value || o.name || o.nameEn;
+          o.nameEn = o.nameEn;
+          if (this.lang)
+            o.value = this.$i18n.locale === "zh" ? o.value : o.nameEn;
+          return o;
+        });
       });
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style lang='scss' scoped>
 </style>
