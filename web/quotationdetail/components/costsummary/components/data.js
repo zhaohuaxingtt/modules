7d41b1924ae3998b7bd07a3797c2e23c338b9c92
余继鹏 <917955345@qq.com>
2023-02-16 17:52:38 +0800

    CRW-12214

--- web/quotationdetail/components/costsummary/components/data.js
+++ web/quotationdetail/components/costsummary/components/data.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-04-23 15:34:54
- * @LastEditTime: 2022-02-17 12:46:30
- * @LastEditors: YoHo
+ * @LastEditTime: 2023-02-16 17:48:25
+ * @LastEditors: 余继鹏 917955345@qq.com
  * @Description: In User Settings Edit
  * @FilePath: \front-modules\web\quotationdetail\components\costsummary\components\data.js
  */
@@ -44,9 +44,12 @@ export function tableTilel1Fn(level, partType, partname) {
       { props: 'ckdManageRate', name: 'CKD件管理费率', key: 'LK_CKDJIANGUANLIFEILV', tooltip: false, width: '', type: 'inputRate' },
     ] : []),
     // 供应商配件与附件的包装运输页面移除，报价成本汇总页面能够直接填写[原材料/散件成本][制造成本][报废成本][管理费][利润][包装费][运输费][操作费]，
-    { props: 'packageCost', name: '包装费', key: 'LK_BAOZHUANGFEI', tooltip: false, width: '', edit: true, type: bnkValid(partname, partType) ? 'input' : '', inputType: 'decimal' },
-    { props: 'transportCost', name: '运输费', key: 'LK_YUNSHUFEI', tooltip: false, width: '', edit: true, type: bnkValid(partname, partType) ? 'input' : '', inputType: 'decimal' },
-    { props: 'operateCost', name: '操作费', key: 'LK_CAOZUOFEI', tooltip: false, width: '', edit: true, type: bnkValid(partname, partType) ? 'input' : '', inputType: 'decimal' },
+    // { props: 'packageCost', name: '包装费', key: 'LK_BAOZHUANGFEI', tooltip: false, width: '', edit: true, type: bnkValid(partname, partType) ? 'input' : '', inputType: 'decimal' },
+    // { props: 'transportCost', name: '运输费', key: 'LK_YUNSHUFEI', tooltip: false, width: '', edit: true, type: bnkValid(partname, partType) ? 'input' : '', inputType: 'decimal' },
+    // { props: 'operateCost', name: '操作费', key: 'LK_CAOZUOFEI', tooltip: false, width: '', edit: true, type: bnkValid(partname, partType) ? 'input' : '', inputType: 'decimal' },
+    { props: 'packageCost', name: '包装费', key: 'LK_BAOZHUANGFEI', tooltip: false, width: '', type: bnkValid(partname, partType) ? 'input' : '', inputType: 'decimal' },
+    { props: 'transportCost', name: '运输费', key: 'LK_YUNSHUFEI', tooltip: false, width: '', type: bnkValid(partname, partType) ? 'input' : '', inputType: 'decimal' },
+    { props: 'operateCost', name: '操作费', key: 'LK_CAOZUOFEI', tooltip: false, width: '', type: bnkValid(partname, partType) ? 'input' : '', inputType: 'decimal' },
     { props: 'totalPriceBprice', name: 'B价', key: 'LK_BJIA', tooltip: false, width: '' },
   ]
 }
